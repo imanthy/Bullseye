fb90a8cdf003f43ca3eccea3a29a1bb6387cf552
Anthy Chen <koyun.chen@gmail.com>
2023-02-01 15:11:57 -0800

    Initial commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Anthy Chen on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
